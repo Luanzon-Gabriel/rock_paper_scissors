b72d44df17ff3f6c60e04b2b12d07b9c0423c887
Gabriel <you@example.com>
2024-10-04 01:49:43 +0800

    NILAGAY NATIN SIYA SA LOOP PARA DI NA KAILANGAN PINDUTIN YUNG RUN PARA MAGLARO

--- rock_paper_scissors_game.py
+++ rock_paper_scissors_game.py
@@ -1,28 +1,30 @@
 import random 
 
 options = ("rock", "paper", "scissors")
-player = None
-computer = random.choice(options)
 running = True
 
+while running:
 
-while player not in options:
-     player = input("Enter a choice (rock, paper, scissors): ")
+    player = None
+    computer = random.choice(options)
 
-print(f"Player: {player}")
-print(f"Computer: {computer}")
+    while player not in options:
+        player = input("Enter a choice (rock, paper, scissors): ")
 
-if player == computer:
-        print("It's a tie!")
-elif player == "rock" and computer == "scissors":
-        print("You win!")
-elif player == "paper" and computer == "rock":
-        print("You win!")
-elif player == "scissors" and computer == "paper":
-        print("You win!")
-else:
-        print("You lose!")
+    print(f"Player: {player}")
+    print(f"Computer: {computer}")
 
-play_again = input("Play again? (y/n): ").lower()
-if not play_again == "y":
-                running = False
+    if player == computer:
+            print("It's a tie!")
+    elif player == "rock" and computer == "scissors":
+            print("You win!")
+    elif player == "paper" and computer == "rock":
+            print("You win!")
+    elif player == "scissors" and computer == "paper":
+            print("You win!")
+    else:
+            print("You lose!")
+
+    play_again = input("Play again? (y/n): ").lower()
+    if not play_again == "y":
+                    running = False
