716fe520c1b4458663807968389a2999cc5585b6
Gabriel <you@example.com>
2024-10-04 01:43:12 +0800

    NILAGYAN NA NG DO YOU WANT TO PLAY AGAIN OPTION

--- rock_paper_scissors_game.py
+++ rock_paper_scissors_game.py
@@ -3,20 +3,26 @@ import random
 options = ("rock", "paper", "scissors")
 player = None
 computer = random.choice(options)
+running = True
+
 
 while player not in options:
-  player = input("Enter a choice (rock, paper, scissors): ")
+     player = input("Enter a choice (rock, paper, scissors): ")
 
 print(f"Player: {player}")
 print(f"Computer: {computer}")
 
 if player == computer:
-    print("It's a tie!")
+        print("It's a tie!")
 elif player == "rock" and computer == "scissors":
-    print("You win!")
+        print("You win!")
 elif player == "paper" and computer == "rock":
-    print("You win!")
+        print("You win!")
 elif player == "scissors" and computer == "paper":
-    print("You win!")
+        print("You win!")
 else:
-    print("You lose!")
+        print("You lose!")
+
+play_again = input("Play again? (y/n): ").lower()
+if not play_again == "y":
+                running = False
